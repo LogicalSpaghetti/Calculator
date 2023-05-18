f3a1c2d6a2dbd0cd3405950936af09b474c40cce
LogicalSpaghetti <logicalspaghetti@gmail.com>
2023-05-18 19:27:15 -0400

    These were useless and I don't feel like making use of them because I've spent long enough on this silly calculator.

--- src/main/java/me/spaghetti/main/JButtonToMathButton.java
+++
@@ -1,6 +0,0 @@
-package me.spaghetti.main;
-
-public class JButtonToMathButton {
-    //called when the input is from a JButton, 
-    //it then translates that button press into the relative key and calls on mathButtons.buttons     
-}
