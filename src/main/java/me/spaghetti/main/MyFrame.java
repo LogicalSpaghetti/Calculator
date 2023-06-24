70ce8127c1d545a41ba9b351e47e3820bcccaad0
LogicalSpaghetti <logicalspaghetti@gmail.com>
2023-06-24 11:30:51 -0400

    commit

---
+++ src/main/java/me/spaghetti/main/MyFrame.java
@@ -0,0 +1,14 @@
+package me.spaghetti.main;
+
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setLayout(null);
+
+        this.setVisible(true);
+    }
+}
